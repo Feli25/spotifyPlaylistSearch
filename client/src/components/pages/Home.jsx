19518b6b724048de8bd4ef13efceb9405485e1f3
Felicitas Deutges <deutges.ironhack@gmail.com>
2020-05-21 10:37:48 +0200

    update text on home screen

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import api from '../../api'
+
 
 export default class Home extends Component {
   render() {
+    var loggedIn = api.isLoggedIn()
     return (
       <div className="Home">
-        <h2>Willkommen</h2>
-        <p>Dies ist eine Basic App mit dem MERN stack und Passport</p>
+        <h2>{loggedIn ? "Wilkommen zurück " + api.getLocalStorageUser().username : "Willkommen"}</h2>
+        {loggedIn ? <React.Fragment>
+          <p>Um nach neuen Playlists zu suchen, klicke auf <a href="/search">"Suche"</a></p>
+          <p>Um dein Profil zu bearbeiten, klicke auf <a href="myProfile">"Mein Profil"</a></p></React.Fragment> :
+          <p>Um diese App zu nutzen, musst du bitte ein Profil erstellen!</p>}
       </div>
     )
   }
