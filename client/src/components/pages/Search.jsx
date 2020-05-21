e1b8d94e9041197c365573028dea04b32c90704b
Felicitas Deutges <deutges.ironhack@gmail.com>
2020-05-21 20:44:35 +0200

    styling improved on search

--- client/src/components/pages/Search.jsx
+++ client/src/components/pages/Search.jsx
@@ -7,7 +7,8 @@ export default class Search extends Component {
     searchField: "",
     results: [],
     searchType: "name",
-    message: null
+    message: null,
+    selectedPlaylist: null
   }
   onChange = (e) => {
     this.setState({ [e.target.name]: e.target.value })
@@ -15,32 +16,53 @@ export default class Search extends Component {
   fetchData = async () => {
     this.setState({ message: "Lädt..." })
     var results = await api.getPlaylists(this.state.searchField, this.state.searchType)
-    console.log("result", results)
     if (results && results.data) {
       if (this.state.searchType === "name" && results.data.playlists) {
-        console.log(results.data.playlists.items)
         this.setState({ results: results.data.playlists.items, message: null })
       } else {//6u4KuddLp926mEZHqCOQwg
-        console.log(results.data.tracks.items)
         this.setState({ results: [results.data], message: null })
       }
     }
   }
   render() {
+    var selectedPlaylist = this.state.selectedPlaylist
     return (
       <div className="MainContent">
-        Suche nach:
+        {selectedPlaylist ?
+          <React.Fragment>
+            {selectedPlaylist.images && selectedPlaylist.images[0] &&
+              <img src={selectedPlaylist.images[0].url} alt="playlistImage" />}
+            <div>
+              <p><strong>Name:</strong> {selectedPlaylist.name}</p>
+              <p><strong>Beschreibung:</strong> {selectedPlaylist.description}</p>
+              <p><strong>Besitzer:</strong> {selectedPlaylist.owner.display_name}</p>
+            </div>
+            <button><a href={selectedPlaylist.external_urls.spotify}>Zu Spotify</a></button><br />
+            <button onClick={() => { this.setState({ selectedPlaylist: null }) }}>Schließen</button>
+          </React.Fragment> :
+          <React.Fragment>
+            Suche nach:
         <div className="selectTypeWrapper">
-          <div className={this.state.searchType === "name" ? "selected selectTypeOption" : "notSelected selectTypeOption"} onClick={() => { this.setState({ searchType: "name" }) }}>Name</div>
-          <div className={this.state.searchType === "id" ? "selected selectTypeOption" : "notSelected selectTypeOption"} onClick={() => { this.setState({ searchType: "id" }) }}>Playlist ID</div>
-        </div>
-        <input name="searchField" value={this.state.searchField} onChange={this.onChange} />
-        {this.state.message ? <p>{this.state.message}</p> : <button onClick={this.fetchData}>Suche</button>}
-        <div className="searchResults">
-          {this.state.results && this.state.results.map(playlist => {
-            return <div>{playlist.name}Zu Spotify</div>
-          })}
-        </div>
+              <div className={this.state.searchType === "name" ? "selected selectTypeOption" : "notSelected selectTypeOption"} onClick={() => { this.setState({ searchType: "name" }) }}>Name</div>
+              <div className={this.state.searchType === "id" ? "selected selectTypeOption" : "notSelected selectTypeOption"} onClick={() => { this.setState({ searchType: "id" }) }}>Playlist ID</div>
+            </div>
+            <input name="searchField" value={this.state.searchField} onChange={this.onChange} />
+            {this.state.message ? <p>{this.state.message}</p> : <button onClick={this.fetchData}>Suche</button>}
+            {this.state.results && this.state.results.length > 0 && <div className="searchResults">
+              <table>
+                <thead>
+                  <th>Name</th>
+                  <th>Mehr Infos</th>
+                </thead>
+                {this.state.results && this.state.results.map(playlist => {
+                  return <tbody key={playlist.id}>
+                    <td>{playlist.name}</td>
+                    <td><button className="moreBtn" onClick={() => { this.setState({ selectedPlaylist: playlist }) }}>Mehr</button></td>
+                  </tbody>
+                })}
+              </table>
+            </div>}</React.Fragment>
+        }
       </div >
     )
   }
