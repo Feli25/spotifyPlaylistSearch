a5fc9cde2c650af32bcd736b153ce3a5d4836459
Felicitas Deutges <deutges.ironhack@gmail.com>
2020-05-21 10:42:30 +0200

    add new files and routes search and profile

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,6 +3,8 @@ import { Route, Link, NavLink, Switch } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Search from './pages/Search'
+import Profile from './pages/Profile'
 import api from '../api'
 
 export default class App extends Component {
@@ -29,6 +31,8 @@ export default class App extends Component {
           </NavLink>
           {!loggedIn && <NavLink to="/signup">Registrieren</NavLink>}
           {!loggedIn && <NavLink to="/login">Einloggen</NavLink>}
+          {loggedIn && <NavLink to="/search">Suche</NavLink>}
+          {loggedIn && <NavLink to="/profile">Mein Profil</NavLink>}
           {loggedIn && (
             <Link to="/" onClick={e => this.handleLogoutClick(e)}>
               Ausloggen
@@ -40,6 +44,8 @@ export default class App extends Component {
             <Route path="/" exact component={Home} />
             <Route path="/signup" component={loggedIn ? Home : Signup} />
             <Route path="/login" component={loggedIn ? Home : Login} />
+            <Route path="/search" component={loggedIn ? Search : Home} />
+            <Route path="/profile" component={loggedIn ? Profile : Home} />
             <Route render={() => <h2>404</h2>} />
           </Switch>
         </div>
---
+++ client/src/components/pages/Search.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class Search extends Component {
+  render() {
+    return (
+      <div className="Search">
+        Search
+      </div>
+    )
+  }
+}
